184a9d7d5264b1a8e5715a69123c915d4c99a83a
Adityagaming <110168105+Adityapanchalgaming@users.noreply.github.com>
2022-10-01 19:28:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("My first console project")
+
+var firstname="Aditya"
+
+var lastname="Panchal"
+
+var fullname=firstname.concat(lastname)
+console.log(fullname)
+
+var a=10
+
+var b = 5
+
+var add =a+b
+console.log(add)
+ 
+var sub= a-b;
+console.log(sub)
+
+var multipli =a*b
+console.log(multipli)
+
+var divide=a/b
+console.log(divide)
